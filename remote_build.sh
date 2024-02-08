6714306d7fb969f2b36b70cf92b38a42554af236
Brais Moure <mouredev@gmail.com>
2024-02-08 12:53:50 +0100

    Script de build remota

---
+++ remote_build.sh
@@ -0,0 +1,11 @@
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+cp vercel.json public/
+deactivate
